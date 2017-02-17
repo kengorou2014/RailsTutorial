0a74b3b8dc06076c1ab8cfa51215c2f11ee201ac
小沢健悟 <kengorou@ozawakengo-no-MacBook-Air.local>
2017-02-17 10:14:56 +0900

    Add account activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,7 +22,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information with account activation" do
   	get signup_path
   	assert_difference 'User.count', 1 do
-  		post users_path, params: {user: { name: "Example User",
+  		post users_path, params: { user: { name: "Example User",
   										  email:"user@example.com", 
   										  password: "password",
   										  password_confirmation: "password"}}
@@ -34,13 +34,13 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   	log_in_as(user)
   	assert_not is_logged_in?
   	#有効かトークンが不正な場合
-  	get edit_account_activation_path("inavlid token", email:user.email)
+  	get edit_account_activation_path("inavlid token", email: user.email)
   	assert_not is_logged_in?
   	#トークンは正しいが窓が無効な場合
-  	get edit_account_activation_path(user.activation_token, email:'wrong')
+  	get edit_account_activation_path(user.activation_token, email: 'wrong')
   	assert_not is_logged_in?
   	#有効かトークンが正しい場合
-  	get edit_account_activation_path(user.activation_token,email:user.email)
+  	get edit_account_activation_path(user.activation_token, email: user.email)
   	assert user.reload.activated?
 	follow_redirect!
 	assert_template 'users/show'
